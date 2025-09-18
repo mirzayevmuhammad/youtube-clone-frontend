4e6efc2462782917b22cf9ca75a734dadbb2e820
Mirzayev Muhammad <mirzayevmuhammad207@gmail.com>
2025-09-18 18:06:48 +0500

    update

--- src/page/homepage.tsx
+++ src/page/homepage.tsx
@@ -1,3 +1,68 @@
+// import { useEffect, useState } from "react";
+// import api from "../api/axios";
+// import Video from "../components/Video";
+
+// interface VideoType {
+//   id: string;
+//   title: string;
+//   description?: string;
+//   duration?: string;
+//   thumbnail?: string;
+//   videoUrl?: string;
+//   viewsCount?: number;
+// }
+
+// const Homepage = () => {
+//   const [videos, setVideos] = useState<VideoType[]>([]);
+
+//   async function fetchData() {
+//     try {
+//       console.log("am");
+
+//       let res = await api.get("/video/feed/videos", {
+//         params: {
+//           limit: 10,
+//           page: 1,
+//           categoryId: "", // agar categoryId kerak bo‘lmasa yubormasa ham bo‘ladi
+//         },
+//       });
+
+//       console.log(res, "am2");
+//       if (res.data && res.data.success) {
+//         setVideos(res.data.data as VideoType[]);
+//       }
+//     } catch (error) {
+//       console.error("Xatolik:", error);
+//     }
+//   }
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="pl-[15px] flex gap-x-5 flex-wrap">
+//         <Video description="" url="" hour="" img="" name="" view="" />
+//         {videos.map((video) => (
+//           <a key={video.id} href={`/watch/${video.id}`}>
+//             <Video
+//               description={video.description || ""}
+//               hour={video.duration || ""}
+//               img={video.thumbnail || ""}
+//               name={video.title || ""}
+//               url={video.videoUrl || ""}
+//               view={(video.viewsCount || 0).toString()}
+//             />
+//           </a>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Homepage;
+
 import { useEffect, useState } from "react";
 import api from "../api/axios";
 import Video from "../components/Video";
@@ -17,10 +82,10 @@ const Homepage = () => {
 
   async function fetchData() {
     try {
-      console.log("am");
+      let res = await api.get("/video/feed/videos", {
+        params: { page: 1, limit: 12 },
+      });
 
-      let res = await api.get("/videos/all");
-      console.log(res, "am2");
       if (res.data && res.data.success) {
         setVideos(res.data.data as VideoType[]);
       }
@@ -34,23 +99,20 @@ const Homepage = () => {
   }, []);
 
   return (
-    <>
-      <div className="pl-[15px] flex gap-x-5 flex-wrap">
-      <Video description="" url="" hour="" img="" name="" view=""/>
-        {videos.map((video) => (
-          <a key={video.id} href={`/watch/${video.id}`}>
-            <Video
-              description={video.description || ""}
-              hour={video.duration || ""}
-              img={video.thumbnail || ""}
-              name={video.title || ""}
-              url={video.videoUrl || ""}
-              view={(video.viewsCount || 0).toString()}
-            />
-          </a>
-        ))}
-      </div>
-    </>
+    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {videos.map((video) => (
+        <a key={video.id} href={`/watch/${video.id}`}>
+          <Video
+            description={video.description || ""}
+            hour={video.duration || ""}
+            img={`http://localhost:4000/${video.thumbnail}`}
+            name={video.title || ""}
+            url={`http://localhost:4000/${video.videoUrl}`}
+            view={(video.viewsCount || 0).toString()}
+          />
+        </a>
+      ))}
+    </div>
   );
 };
 
